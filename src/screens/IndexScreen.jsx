b8f56aab9b8fe6d1aab0c23e40f067a87017364c
Daniel Stephenson <daniel@giffgaff.co.uk>
2020-01-03 16:44:36 +0000

    Manage blogPost state with React Context

--- src/screens/IndexScreen.jsx
+++ src/screens/IndexScreen.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, FlatList, Button } from 'react-native';
+import BlogContext from '../context/BlogContent';
 
 const IndexScreen = () => {
+  const { data, addBlogPost } = useContext(BlogContext);
   return (
     <View>
       <Text>Index Screen</Text>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={data}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
     </View>
   );
 };
