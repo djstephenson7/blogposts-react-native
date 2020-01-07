dcfeaf934fbf4fdfe2a24b5d8aff9d3e68ed1c0e
djstephenson7 <dan_0502@hotmail.co.uk>
2020-01-07 21:13:04 +0000

    Add API calls to CRUD actions

--- src/context/BlogContext.jsx
+++ src/context/BlogContext.jsx
@@ -5,27 +5,21 @@ const blogReducer = (state, action) => {
   switch (action.type) {
     case 'get_blogposts':
       return action.payload;
+
     case 'edit_blogpost':
       return state.map(blogPost => {
         return blogPost.id === action.payload.id ? action.payload : blogPost;
       });
+
     case 'delete_blogpost':
       return state.filter(blogPost => blogPost.id !== action.payload);
-    case 'add_blogpost':
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 9999),
-          title: action.payload.title,
-          content: action.payload.content
-        }
-      ];
+
     default:
       return state;
   }
 };
 
-getBlogsPosts = dispatch => {
+getBlogPosts = dispatch => {
   return async () => {
     const response = await jsonServer.get('/blogposts');
 
@@ -34,27 +28,32 @@ getBlogsPosts = dispatch => {
 };
 
 const addBlogPost = dispatch => {
-  return (title, content, callback) => {
-    dispatch({ type: 'add_blogpost', payload: { title, content } });
+  return async (title, content, callback) => {
+    await jsonServer.post('/blogposts', { title, content });
+
     if (callback) callback();
   };
 };
 
 const deleteBlogPost = dispatch => {
-  return id => {
+  return async id => {
+    await jsonServer.delete(`/blogposts/${id}`);
+
     dispatch({ type: 'delete_blogpost', payload: id });
   };
 };
 
 const editBlogPost = dispatch => {
-  return (id, title, content, callback) => {
+  return async (id, title, content, callback) => {
+    await jsonServer.put(`/blogposts/${id}`, { title, content });
     dispatch({ type: 'edit_blogpost', payload: { id, title, content } });
+
     if (callback) callback();
   };
 };
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost, editBlogPost, getBlogsPosts },
+  { addBlogPost, deleteBlogPost, editBlogPost, getBlogPosts },
   []
 );
